589f1363c41e0bef226bf4088f9b362f39e6fe12
Stepan Kormilin <ubscoder@gmail.com>
2025-03-23 20:36:55 +0300

    refactoring && fix

--- src/components/Counter/useCounter.js
+++ src/components/Counter/useCounter.js
@@ -1,22 +1,23 @@
 import { useState } from 'react'
 
 export const useCounter = (minCount, maxCount) => {
-  const [count, setCount] = useState(0);
+  const [count, setCount] = useState(0)
   
   const increment = () => {
     if (count < maxCount) {
-      setCount(count + 1);
+      setCount(count + 1)
     }
-  };
+  }
+  
   const decrement = () => {
     if (count > minCount) {
-      setCount(count - 1);
+      setCount(count - 1)
     }
-  };
+  }
   
   return {
     count,
     increment,
     decrement,
-  };
+  }
 }
