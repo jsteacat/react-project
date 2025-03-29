a91b68ca3f77729114ef29d0a2d8fefac89e669c
Stepan Kormilin <ubscoder@gmail.com>
2025-03-29 11:30:26 +0300

    add main page

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,44 +1,16 @@
-import { useState } from 'react'
 import { Layout } from '../Layout/Layout.jsx'
-import { restaurants } from '../../mock.js'
 import { ThemeContext } from '../ThemeContext/ThemeContext.jsx'
-import { Restaurant } from '../Restaurant/Restaurant.jsx'
-import { Tabs } from '../Tabs/Tabs.jsx'
 import { UserContext } from '../UserContext/UserContext.jsx'
+import { MainPage } from '../MainPage/MainPage.jsx'
 
 export const App = ({ title }) => {
-  const [selectedRestaurantId, setSelectedRestaurantId] = useState(restaurants.length > 0 ? restaurants[0].id : null)
-  const selectedRestaurant = restaurants.find((restaurant) => restaurant.id === selectedRestaurantId) ?? null
-  const tabs = restaurants.map(({ id, name }) => ({
-    title: name,
-    value: id,
-    active: id === selectedRestaurantId,
-  }))
-
   return (
     <ThemeContext>
       <UserContext>
         <Layout>
           <h1>{title}</h1>
           <br />
-          {restaurants.length > 0 ? (
-            <div className="content">
-              <Tabs tabs={tabs} onClick={setSelectedRestaurantId} />
-              <br />
-              {Boolean(selectedRestaurant) && (
-                <>
-                  {Array.from({ length: 5 }, (_, index) => (
-                    <div key={selectedRestaurant + index}>
-                      <Restaurant restaurant={selectedRestaurant} />
-                      <br />
-                    </div>
-                  ))}
-                </>
-              )}
-            </div>
-          ) : (
-            <div className="empty-data">Данных нет</div>
-          )}
+          <MainPage />
         </Layout>
       </UserContext>
     </ThemeContext>
